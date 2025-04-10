4b698db5020934ea80e72f213e124af4aefe19fb
Marc Farra <marcfarra@gmail.com>
2025-04-10 19:10:55 +0300

    fix session in middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,60 @@
+import { defineMiddleware } from "astro:middleware";
+import { supabase } from "../db/supabase";
+import micromatch from "micromatch";
+
+const protectedRoutes = ["/admin(|/)"];
+const redirectRoutes = ["/admin/login(|/)"];
+
+export const onRequest = defineMiddleware(
+  async ({ locals, url, cookies, redirect }, next) => {
+    if (micromatch.isMatch(url.pathname, protectedRoutes)) {
+      console.log("Protected route");
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+      console.log("Access token:", accessToken);
+      console.log("Refresh token:", refreshToken);
+
+      if (!accessToken || !refreshToken) {
+        return redirect("/admin/login");
+      }
+
+      const { data, error } = await supabase.auth.setSession({
+        refresh_token: refreshToken.value,
+        access_token: accessToken.value,
+      });
+
+      if (error) {
+        cookies.delete("sb-access-token", {
+          path: "/",
+        });
+        cookies.delete("sb-refresh-token", {
+          path: "/",
+        });
+        return redirect("/admin/login");
+      }
+
+      locals.email = data.user?.email!;
+      cookies.set("sb-access-token", data?.session?.access_token!, {
+        sameSite: "strict",
+        path: "/",
+        secure: true,
+      });
+      cookies.set("sb-refresh-token", data?.session?.refresh_token!, {
+        sameSite: "strict",
+        path: "/",
+        secure: true,
+      });
+    }
+
+    if (micromatch.isMatch(url.pathname, redirectRoutes)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      if (accessToken && refreshToken) {
+        return redirect("/admin");
+      }
+    }
+
+    return next();
+  }
+);
